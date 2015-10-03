ab488273038b783b668d3249f32096c119de4b02
KeiZar <matrixworlds@gmail.com>
2015-10-03 15:06:50 +0300

    Revert "-Added Menu and Player Control -Implemented Tower placement control, but no actual towers -Implemented a 0.1 version of the Highscore table -Implemented"
    This reverts commit dab706a43f6d9b7816b5c516933aa2a6d288e0a1.

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,140 +9,11 @@ namespace Game
 {
     class Program
     {
-        static int playerStartPossitionX = 0;
-        static int playerStartPossitionY = 0;
-        static int playerCurrentPossitionX = 0;
-        static int playerCurrentPossitionY = 0;
-        static bool isRuning = true;
-        static bool startNewGame = false;
-        static int showMenus = 0; // 0 - main menu, 1 - highscore table, 2 - How to play
-
-        static void RemoveScrollBars() // Sets window size and locks it in place.
-        {
-            Console.WindowHeight = 50;
-            Console.WindowWidth = 100;
-            Console.BufferHeight = 50;
-            Console.BufferWidth = 100;
-        }
-
-        private static void MenuControl(ConsoleKey key) // TODO: Needs adjusting with Menu screens
-        {
-            /*
-            While in Main menu controls are:
-            Z - start new game
-            X - Highscore Table
-            C - How to play
-            V - Exit game/back button
-            */
-            if (key == ConsoleKey.Z)
-            {
-                startNewGame = true;
-            }
-            if (key == ConsoleKey.X)
-            {
-                // TODO: Show highscore table
-                
-                if (showMenus == 0 || showMenus == 2)
-                {
-                    showMenus = 1;
-                    ShowHighscore();
-                }
-            }
-            if (key == ConsoleKey.C)
-            {
-                // TODO: Show "How to play screen"
-                if (showMenus == 0 || showMenus == 1)
-                {
-                    showMenus = 2;
-                }
-            }
-            if (key == ConsoleKey.V)
-            {
-                if (showMenus == 0)
-                {
-                    isRuning = false;
-                }
-                else if (showMenus == 1 || showMenus == 2)
-                {
-                    showMenus = 0;
-                }
-            }
-        }
-
-        private static void ShowHighscore()
-        {
-            
-            Console.WriteLine("**HIGHSCORE TABLE**");
-            // TODO: Implement reading FROM a file and writing on the console!
-
-           
-        }
-
-        private static void PlaceTowers(ConsoleKey key)
-        {
-            if (key == ConsoleKey.NumPad1)
-            {
-                // TODO: Place tower #1 on player possition
-            }
-            if (key == ConsoleKey.NumPad2)
-            {
-                // TODO: Place tower #2 on player possition
-            }
-            if (key == ConsoleKey.NumPad3)
-            {
-                // TODO: Place tower #3 on player possition
-            }
-            if (key == ConsoleKey.NumPad4)
-            {
-                // TODO: Place tower #4 on player possition
-            }
-        } // TODO: Implement tower placement
-
-        private static void MovePlayer(ConsoleKey key) //Player movement - needs adjusting if place size bigger then 1 symbol TODO: Detect collision with station!
-        {
-            if (key == ConsoleKey.UpArrow)
-            {
-                if (playerCurrentPossitionX > Console.WindowHeight)
-                {
-                    playerCurrentPossitionX--;
-                }
-
-            }
-            if (key == ConsoleKey.DownArrow)
-            {
-                if (playerCurrentPossitionX < Console.WindowHeight)
-                {
-                    playerCurrentPossitionX++;
-                }
-
-            }
-            if (key == ConsoleKey.LeftArrow)
-            {
-                if (playerCurrentPossitionY > Console.WindowWidth)
-                {
-                    playerCurrentPossitionY--;
-                }
-            }
-            if (key == ConsoleKey.RightArrow)
-            {
-                if (playerCurrentPossitionY < Console.WindowWidth)
-                {
-                    playerCurrentPossitionY++;
-
-                }
-            }
-        }
-
-
         static void Main(string[] args)
         {
-            RemoveScrollBars();
-
-
-
+            bool isRuning = true;
             while (isRuning)
             {
-
                 /*
                 Everything is more or less a lot of if-else checks and a lot of Console.Writeline();
                 Be mindful of the other parts of the code and ask questions on Skype or if you need help!
@@ -152,32 +21,11 @@ namespace Game
                 */
 
                 /*
-                TODO: Implement Tower placement function
-                TODO: Towers
-                TODO: Implement 4 different towers [See Trello for more details]
+                TODO: Keyboard controls
+                TODO: Implement Tower placement buttons
                 */
-                //Prevents Console getting stuck on waiting for a key press, it only triggered when a key is pressed.
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo keyInfo = Console.ReadKey();
-                    if (startNewGame)
-                    {
-                        MovePlayer(keyInfo.Key);
-                        PlaceTowers(keyInfo.Key); // Implement towers!
-                    }
-                    else
-                    {
-                        MenuControl(keyInfo.Key);
-                    }
-
-
-                }
-
-                Console.Clear();
-                // TODO: Implement "DrawMenu();" else Console.Clear(); just wipes the console entirely
 
                 /*
-                CHECK MENU CONTROL METHOD ABOVE!!!
                 TODO: Menu
                 TODO: Implement "Start new game"
                 TODO: Implement "Highscore" option
@@ -195,21 +43,17 @@ namespace Game
                 */
 
                 /*
-                TODO: Map
-                TODO: Implement Station possition on console + HP of station
-                TODO: Player start possition + player collision with station - see player movement method.
+                TODO: Towers
+                TODO: Implement 4 different towers [See Trello for more details]
                 */
 
                 /*
-                TODO: At game end, show on screen score and first 3 high score possitions!
-                TODO: If new score is > from current highscores, add and move possitions!
-                TODO: Write to file the highscores!
+                TODO: Map
+                TODO: Implement Station possition on console + HP of station
+                TODO: Player start possition
                 */
-
                 Thread.Sleep(60);
             }
         }
-
-
     }
 }

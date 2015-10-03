7e5cc428f9d42ec25a59d263d8cec367473d07c8
KeiZar <matrixworlds@gmail.com>
2015-10-03 13:26:05 +0300

    First commit

---
+++ Game/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRuning = true;
+            while (isRuning)
+            {
+                /*
+                Everything is more or less a lot of if-else checks and a lot of Console.Writeline();
+                Be mindful of the other parts of the code and ask questions on Skype or if you need help!
+                We will make a lot of static methods and we will only write them in this file. No OOP elements!
+                */
+
+                /*
+                TODO: Keyboard controls
+                TODO: Implement Tower placement buttons
+                */
+
+                /*
+                TODO: Menu
+                TODO: Implement "Start new game"
+                TODO: Implement "Highscore" option
+                TODO: Implement "How to play" option
+                TODO: Implement stop function [Use "Environment.Exit(-1);" in menu]
+                */
+
+
+
+                /*
+                TODO: Enemy movement
+                TODO: Implement enemy spawn and movement
+                TODO: Implement enemy HP, Points and gold drop                
+                TODO: Implement ground and air only enemies
+                */
+
+                /*
+                TODO: Towers
+                TODO: Implement 4 different towers [See Trello for more details]
+                */
+
+                /*
+                TODO: Map
+                TODO: Implement Station possition on console + HP of station
+                TODO: Player start possition
+                */
+                Thread.Sleep(60);
+            }
+        }
+    }
+}

cc8bdbe21020198bd585e49504f20ec1cf22d705
KeiZar <matrixworlds@gmail.com>
2015-10-06 21:50:29 +0300

    erger

--- Game/GalaxyStation.cs
+++ Game/GalaxyStation.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+namespace Game
+{
 
     class GalaxyStation
     {
-        static void Main()
+        static void DrawStation()
         {
             int n = 10;
 
@@ -15,119 +13,121 @@ using System.Threading.Tasks;
                 new string(' ', n),
                 new string('-', n));
 
-             Console.WriteLine("{0}/{2}\\{1}",
+            Console.WriteLine("{0}/{2}\\{1}",
+               new string(' ', n - 1),
+               new string(' ', n - 1),
+               new string(' ', n));
+
+            Console.WriteLine("{0}\\{2}/{1}",
                 new string(' ', n - 1),
                 new string(' ', n - 1),
                 new string(' ', n));
 
-             Console.WriteLine("{0}\\{2}/{1}",
-                 new string(' ', n - 1),
-                 new string(' ', n - 1),
-                 new string(' ', n));
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n),
+                new string('-', n));
 
-             Console.WriteLine("{0}{1}{0}",
-                 new string(' ', n),
-                 new string('-', n));
+            Console.WriteLine("{0}/{2}\\{1}",
+                     new string(' ', n + 1),
+                     new string(' ', n - 1),
+                     new string(' ', n / 2 + 1));
 
-             Console.WriteLine("{0}/{2}\\{1}",
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}|{2}|{1}",
+                    new string(' ', n + 1),
+                    new string(' ', n + 1),
+                    new string(' ', n / 2 + 1));
+            }
+
+            Console.WriteLine("{0}\\{2}/{1}",
                       new string(' ', n + 1),
                       new string(' ', n - 1),
                       new string(' ', n / 2 + 1));
 
-             for (int i = 0; i < n / 2 - 1; i++)
-             {
-                 Console.WriteLine("{0}|{2}|{1}",
-                     new string(' ', n + 1),
-                     new string(' ', n + 1),
-                     new string(' ', n / 2 + 1));
-             }
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n / n + 1),
+                new string('-', n * 3 - 2));
 
-             Console.WriteLine("{0}\\{2}/{1}",
+            Console.WriteLine(" /{0}\\",
+                new string(' ', n * 3 - 2));
+
+            Console.WriteLine(" \\{0}/",
+                new string(' ', n * 3 - 2));
+
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n / n + 1),
+                new string('-', n * 3 - 2));
+
+            Console.WriteLine("{0}/{2}\\{1}",
+                      new string(' ', n + 1),
+                      new string(' ', n - 1),
+                      new string(' ', n / 2 + 1));
+
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}|{2}|{1}",
+                    new string(' ', n + 1),
+                    new string(' ', n + 1),
+                    new string(' ', n / 2 + 1));
+            }
+
+            Console.WriteLine("{0}\\{2}/{1}",
                        new string(' ', n + 1),
                        new string(' ', n - 1),
                        new string(' ', n / 2 + 1));
 
-             Console.WriteLine("{0}{1}{0}",
+            Console.WriteLine("{0}{1}{0}",
                  new string(' ', n / n + 1),
                  new string('-', n * 3 - 2));
 
-             Console.WriteLine(" /{0}\\",
-                 new string(' ', n * 3 - 2));
+            Console.WriteLine(" /{0}\\",
+                new string(' ', n * 3 - 2));
 
-             Console.WriteLine(" \\{0}/",
-                 new string(' ', n * 3 - 2));
+            Console.WriteLine(" \\{0}/",
+                new string(' ', n * 3 - 2));
 
-             Console.WriteLine("{0}{1}{0}",
-                 new string(' ', n / n + 1),
-                 new string('-', n * 3 - 2));
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n / n + 1),
+                new string('-', n * 3 - 2));
 
-             Console.WriteLine("{0}/{2}\\{1}",
+            Console.WriteLine("{0}/{2}\\{1}",
                        new string(' ', n + 1),
                        new string(' ', n - 1),
                        new string(' ', n / 2 + 1));
 
-             for (int i = 0; i < n / 2 - 1; i++)
-             {
-                 Console.WriteLine("{0}|{2}|{1}",
-                     new string(' ', n + 1),
-                     new string(' ', n + 1),
-                     new string(' ', n / 2 + 1));
-             }
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}|{2}|{1}",
+                    new string(' ', n + 1),
+                    new string(' ', n + 1),
+                    new string(' ', n / 2 + 1));
+            }
 
-             Console.WriteLine("{0}\\{2}/{1}",
-                        new string(' ', n + 1),
-                        new string(' ', n - 1),
-                        new string(' ', n / 2 + 1));
+            Console.WriteLine("{0}\\{2}/{1}",
+                       new string(' ', n + 1),
+                       new string(' ', n - 1),
+                       new string(' ', n / 2 + 1));
 
-             Console.WriteLine("{0}{1}{0}",
-                  new string(' ', n / n + 1),
-                  new string('-', n * 3 - 2));
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n),
+                new string('-', n));
 
-             Console.WriteLine(" /{0}\\",
-                 new string(' ', n * 3 - 2));
+            Console.WriteLine("{0}/{2}\\{1}",
+               new string(' ', n - 1),
+               new string(' ', n - 1),
+               new string(' ', n));
 
-             Console.WriteLine(" \\{0}/",
-                 new string(' ', n * 3 - 2));
-
-             Console.WriteLine("{0}{1}{0}",
-                 new string(' ', n / n + 1),
-                 new string('-', n * 3 - 2));
-
-             Console.WriteLine("{0}/{2}\\{1}",
-                        new string(' ', n + 1),
-                        new string(' ', n - 1),
-                        new string(' ', n / 2 + 1));
-
-             for (int i = 0; i < n / 2 - 1; i++)
-             {
-                 Console.WriteLine("{0}|{2}|{1}",
-                     new string(' ', n + 1),
-                     new string(' ', n + 1),
-                     new string(' ', n / 2 + 1));
-             }
-
-             Console.WriteLine("{0}\\{2}/{1}",
-                        new string(' ', n + 1),
-                        new string(' ', n - 1),
-                        new string(' ', n / 2 + 1));
-
-             Console.WriteLine("{0}{1}{0}",
-                 new string(' ', n),
-                 new string('-', n));
-
-             Console.WriteLine("{0}/{2}\\{1}",
+            Console.WriteLine("{0}\\{2}/{1}",
                 new string(' ', n - 1),
                 new string(' ', n - 1),
                 new string(' ', n));
 
-             Console.WriteLine("{0}\\{2}/{1}",
-                 new string(' ', n - 1),
-                 new string(' ', n - 1),
-                 new string(' ', n));
-
-             Console.WriteLine("{0}{1}{0}",
-                 new string(' ', n),
-                 new string('-', n));
+            Console.WriteLine("{0}{1}{0}",
+                new string(' ', n),
+                new string('-', n));
         }
     }
+}
+    
 
--- Game/Program.cs
+++ Game/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Remoting.Channels;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Game
 {
